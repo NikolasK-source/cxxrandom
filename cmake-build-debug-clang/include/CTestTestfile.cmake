fa8758cf036a9d63d0fe9ce1c5f73b1f35cb9818
Nikolas Koesling <nikolas@koesling.info>
2024-03-01 20:13:09 +0100

    remove build artifacts

--- cmake-build-debug-clang/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nk/Development/Privat/_CPP/libs/cxxrandom/include
-# Build directory: /home/nk/Development/Privat/_CPP/libs/cxxrandom/cmake-build-debug-clang/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
