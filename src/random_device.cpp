93e736c1ea313a3e0a70a9d89dcb2c06b6b61e72
Nikolas Koesling <nikolas@koesling.info>
2024-04-22 07:41:46 +0200

    add function to accesss random engine

--- src/random_device.cpp
+++ src/random_device.cpp
@@ -3,9 +3,14 @@
  */
 
 #include "random_device.hpp"
+#include "cxxrandom.hpp"
 
 namespace cxxrandom {
 
 const Rnd rnd;
 
+std::default_random_engine &get_random_engine() {
+    return rnd.random_engine;
 }
+
+}  // namespace cxxrandom
